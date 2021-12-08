52a193b1873b0174cad9ccdbb963bfb131dd6570
pacosanchezm <paco.sanchezm@gmail.com>
2021-12-08 17:31:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 // Replace this file with your actual React component
+
+// This component also requires 'react',
+// but it has already been included in the
+// list of dependencies in package.json
+
+
+
+import React from 'react';
+
+// This component requires prop-types
+import PropTypes from 'prop-types';
+
+import './MyCoolButton.css';
+
+const MyCoolButton = ({ type, title, onClick }) => (
+  <button
+    type={type}
+    className="container"
+    onClick={onClick}
+  >
+    {title}
+  </button>
+);
+
+MyCoolButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+MyCoolButton.defaultProps = {
+  type: 'button',
+  onClick: () => {},
+};
+
+export default MyCoolButton;
+
+
+
