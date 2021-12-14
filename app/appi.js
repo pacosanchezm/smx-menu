e287e16426791d9c464e3649f38130cf4ce9874c
pacosanchezm <paco.sanchezm@gmail.com>
2021-12-14 11:48:39 -0600

    dummy1

--- app/appi.js
+++ app/appi.js
@@ -1,20 +1,81 @@
-import React from "react"
+import React, { useState, useContext, createContext, } from "react"
+import { ThemeProvider, jsx, Styled, useThemeUI } from "theme-ui";
 
-import Menuxa from "../src/index"
+import Menu from "../src/index"
+import Theme from "../src/theme"
 
 
 let App;
+const StateContext = createContext();
+
+
+
+
+
+
+
+
+const useStateUniv = () => {
+  return {
+
+    Menu: {
+      onMenu: useState(useContext(createContext(false))),
+      Selected: useState(useContext(createContext(0))),
+    },
+  }
+}
+
+// ------------------
+
+
+
+// ------------------
+
+const ContextProvider = ({ children }) => {
+  return (
+    <StateContext.Provider value={useStateUniv()}>
+      <ThemeProvider theme={Theme}>{children}</ThemeProvider>
+    </StateContext.Provider>
+  )
+}
+
+// --------------------------------------------------------------------------
+
+
+
+
+const Body = props => {
+
+// ------------
+  try {
+    return (
+      <Menu title="Click Mas!"
+        useContext={useContext(StateContext)}
+      // useAcciones = {useacciones}
+      />
+    )
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
 
 
 
 export default (App = props => {
 
+
+
+
+
   return (
 
     <div id="App">
+      <ContextProvider>
+        <Body {...props} />
 
-      <Menuxa title="Click Mas!"/>
-
+    </ContextProvider>
     </div>
 
   )
